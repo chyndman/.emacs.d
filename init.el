f46ba2ce52e3e64c97f9508578b840c671099649
Chris Hyndman <chrishyndman@hey.com>
2020-11-26 20:20:39 -0800

    better-defaults, stable MELPA, my cosmetic picks

---
+++ init.el
@@ -0,0 +1,19 @@
+;; Copyright (c) 2020, Chris Hyndman
+;; SPDX-License-Identifier: BSD-3-Clause
+
+(add-to-list 'load-path "~/.emacs.d/better-defaults/")
+(require 'better-defaults)
+
+(require 'package)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(unless package-archive-contents (package-refresh-contents))
+
+(dolist (package '(solarized-theme))
+  (unless (package-installed-p package) (package-install package)))
+
+(add-to-list 'default-frame-alist '(font . "Victor Mono-10"))
+(load-theme 'solarized-light t)
+
+(server-start)
