808678dff6808b8df7f6db51524db1f8f84c48ea
Chris Hyndman <chrishyndman@hey.com>
2021-12-20 09:07:34 -0800

    pull in desired defaults, reorg

--- init.el
+++ init.el
@@ -1,13 +1,24 @@
 ;; Copyright (c) 2020, Chris Hyndman
 ;; SPDX-License-Identifier: BSD-3-Clause
 
-;; Start with better-defaults
-(require 'better-defaults "~/.emacs.d/better-defaults/better-defaults.el")
+;; Core UI/behavior (mostly selected from better-defaults)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(horizontal-scroll-bar-mode -1)
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+(show-paren-mode t)
+(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+(setq-default indent-tabs-mode nil)
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(setq backup-directory-alist `(("." . ,(concat user-emacs-directory "backups"))))
+(setq visible-bell t
+      inhibit-startup-screen t
+      auto-save-default nil)
 
-;; Set more in-box variables
-(setq inhibit-startup-screen t
-      auto-save-default nil
-      c-default-style "stroustrup")
+;; Font
 (set-face-attribute 'default nil :family "MonoLisa")
 
 ;; package bootstrap
@@ -30,6 +41,9 @@
 
   (when (package-installed-p 'cider)
     (use-package cider)))
+
+;; C/C++
+(setq c-default-style "stroustrup")
 
 ;; Start server
 (server-start)
