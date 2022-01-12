c1e6c54052e3f5f14882883212e48c544a4760e2
Chris Hyndman <chrishyndman@hey.com>
2022-01-11 23:47:57 -0800

    facelift w/ ivy/counsel

--- init.el
+++ init.el
@@ -2,8 +2,6 @@
 ;; SPDX-License-Identifier: BSD-3-Clause
 
 ;; Core UI/behavior (mostly selected from better-defaults)
-(ido-mode t)
-(setq ido-enable-flex-matching t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
@@ -11,12 +9,12 @@
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
 (show-paren-mode t)
-(global-set-key (kbd "M-/") 'hippie-expand)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (setq-default indent-tabs-mode nil)
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (setq backup-directory-alist `(("." . ,(concat user-emacs-directory "backups"))))
-(setq visible-bell t
+(setq visible-bell nil
+      ring-bell-function 'ignore
       inhibit-startup-screen t
       auto-save-default nil)
 
@@ -36,10 +34,35 @@
 (when (package-installed-p 'use-package)
   (require 'use-package)
 
+  (use-package ivy
+    :ensure t
+    :config
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t
+          enable-recursive-minibuffers t))
+
+  (use-package counsel
+    :after ivy
+    :ensure t
+    :config
+    (counsel-mode 1))
+
+  (use-package swiper
+    :after ivy
+    :ensure t
+    :bind
+    (("C-s" . swiper)
+     ("C-r" . swiper)))
+
   (use-package doom-themes
     :ensure t
     :config
     (load-theme 'doom-vibrant t))
+
+  (use-package markdown-mode
+    :ensure t
+    :init
+    (setq markdown-header-scaling t))
 
   (when (package-installed-p 'cider)
     (use-package cider)))
