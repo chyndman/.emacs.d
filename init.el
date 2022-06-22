1d3ff969575aa3a97d59129225a311c28a369bfc
Chris Hyndman <chrishyndman@hey.com>
2022-06-22 08:19:39 -0700

    switch to helm, overhaul/reflow

--- init.el
+++ init.el
@@ -1,26 +1,24 @@
 ;; Copyright (c) 2020, Chris Hyndman
 ;; SPDX-License-Identifier: BSD-3-Clause
 
-;; Core UI/behavior
+;; Core
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (horizontal-scroll-bar-mode -1)
 (require 'uniquify)
-(setq uniquify-buffer-name-style 'forward)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
 (show-paren-mode t)
 (column-number-mode t)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (setq-default indent-tabs-mode nil)
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(setq backup-directory-alist `(("." . ,(concat user-emacs-directory "backups"))))
-(setq visible-bell nil
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory)
+      backup-directory-alist `(("." . ,(concat user-emacs-directory "backups")))
+      visible-bell nil
       ring-bell-function 'ignore
       inhibit-startup-screen t
       auto-save-default nil
       mouse-wheel-progressive-speed nil)
-(add-to-list 'default-frame-alist '(width . 132))
-(add-to-list 'default-frame-alist '(height . 42))
 
 ;; Theme
 (load-theme 'deeper-blue t)
@@ -32,58 +30,39 @@
         (init-font (cdr fonts))
       (set-face-attribute 'default nil :family (car fonts))
       (set-face-attribute 'fixed-pitch nil :family (car fonts)))))
-(init-font '("MonoLisa" "JetBrains Mono" "Cascadia Code"))
-
-;; package bootstrap
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-(unless package-archive-contents (package-refresh-contents))
-
-;; use-package install
-(unless (package-installed-p 'use-package) (package-install 'use-package))
-
-;; Use packages if use-package is available
-(when (package-installed-p 'use-package)
-  (require 'use-package)
-
-  (use-package ivy
-    :ensure t
-    :config
-    (ivy-mode 1)
-    (setq ivy-use-virtual-buffers t))
-
-  (use-package counsel
-    :after ivy
-    :ensure t
-    :config
-    (counsel-mode 1))
-
-  (use-package swiper
-    :after ivy
-    :ensure t
-    :bind
-    (("C-s" . swiper)
-     ("C-r" . swiper)))
-
-  (use-package markdown-mode
-    :ensure t
-    :init
-    (setq markdown-header-scaling t)
-    :config
-    (set-face-attribute 'markdown-header-face nil :inherit '(fixed-pitch-face font-lock-function-name-face)))
-
-  (when (package-installed-p 'eglot-fsharp)
-    (use-package eglot-fsharp
-      :ensure t
-      :custom
-      (eglot-fsharp-server-version "0.49.6")
-      :config
-      (require 'fsharp-mode)
-      (require 'eglot-fsharp))))
+(init-font '("MonoLisa"
+             "Cascadia Code"))
 
 ;; C/C++
 (setq c-default-style "stroustrup")
 
 ;; Start server
 (server-start)
+
+;; package setup
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(unless package-archive-contents (package-refresh-contents))
+(unless (package-installed-p 'use-package) (package-install 'use-package))
+(require 'use-package)
+
+;; Helm
+(use-package helm
+  :ensure t
+  :bind
+  (("M-x" . helm-M-x)
+   ("C-x r b" . helm-filtered-bookmarks)
+   ("C-x C-f" . helm-find-files)
+   ("C-x C-b" . helm-buffers-list)
+   ("M-s o" . helm-occur))
+  :config
+  (helm-mode 1))
+
+;; Markdown
+(use-package markdown-mode
+  :ensure t
+  :init
+  (setq markdown-header-scaling t)
+  :config
+  (set-face-attribute 'markdown-header-face nil :inherit '(fixed-pitch-face font-lock-function-name-face)))
