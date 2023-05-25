d3c892c93f53a6ac8c91159203a8ec8f9fc68b5c
Chris Hyndman <chrishyndman@hey.com>
2023-05-24 23:06:15 -0700

    init theming w/ onehalf palette

--- init.el
+++ init.el
@@ -26,10 +26,11 @@
   (when fonts
     (if (not (find-font (font-spec :name (car fonts))))
         (init-font (cdr fonts))
-      (set-face-attribute 'default nil :family (car fonts) :height 110)
+      (set-face-attribute 'default nil :family (car fonts))
       (set-face-attribute 'fixed-pitch nil :family (car fonts)))))
 (init-font '("IBM Plex Mono"
              "Cascadia Code"))
+(set-face-attribute 'default nil :height 110)
 
 ;; C/C++
 (setq c-default-style "stroustrup")
@@ -71,3 +72,45 @@
 (autoload 'go-mode "go-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
 (add-hook 'go-mode-hook (lambda () (setq-local tab-width 4)))
+
+;; Colors
+(setq onehalf-light-colors
+  '((black . "#383a42")
+    (red . "#e45649")
+    (green . "#50a14f")
+    (yellow . "#c18401")
+    (blue . "#0184bc")
+    (magenta . "#a626a4")
+    (cyan . "#0997b3")
+    (white . "#fafafa")
+    (comment . "#a0a1a7")
+    (foreground . "#383a42")
+    (background . "#fafafa")
+    (selection . "#bfceff")))
+(setq colors onehalf-light-colors)
+(defun init-bg-color (face color)
+  (set-face-background face (alist-get color colors)))
+(defun init-fg-color (face color)
+  (set-face-foreground face (alist-get color colors)))
+
+;; Syntax Theme
+(init-bg-color 'default 'background)
+(init-fg-color 'default 'foreground)
+(init-fg-color 'font-lock-builtin-face 'magenta)
+(init-fg-color 'font-lock-comment-delimiter-face 'green)
+(init-fg-color 'font-lock-comment-face 'green)
+(init-fg-color 'font-lock-constant-face 'green)
+(init-fg-color 'font-lock-doc-face 'green)
+(init-fg-color 'font-lock-function-name-face 'cyan)
+(init-fg-color 'font-lock-keyword-face 'blue)
+(init-fg-color 'font-lock-preprocessor-face 'magenta)
+(init-fg-color 'font-lock-string-face 'red)
+(init-fg-color 'font-lock-type-face 'cyan)
+(init-fg-color 'font-lock-variable-name-face 'foreground)
+
+;; UI Theme
+(init-bg-color 'region 'selection)
+(init-bg-color 'show-paren-match 'black)
+(init-fg-color 'show-paren-match 'white)
+(init-bg-color 'show-paren-mismatch 'yellow)
+(init-fg-color 'show-paren-mismatch 'white)
