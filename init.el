56484e08cb0c1b44375305e776103d065aaaeb2f
Chris Hyndman <chrishyndman@hey.com>
2023-08-13 18:21:05 -0700

    font exhaustive search, theming w/ terminal

--- init.el
+++ init.el
@@ -21,45 +21,65 @@
 
 ;; Font
 (when (display-graphic-p)
-  (let ((font "Julia Mono"))
-    (when (find-font (font-spec :name font))
-      (set-face-attribute 'default nil :family font)
-      (set-face-attribute 'fixed-pitch nil :family font)))
-  (set-face-attribute 'default nil :height 110))
+  (defun init-font (fonts)
+    (when fonts
+      (if (not (find-font (font-spec :name (car fonts))))
+          (init-font (cdr fonts))
+        (set-face-attribute 'default nil :family (car fonts))
+        (set-face-attribute 'fixed-pitch nil :family (car fonts)))))
+  (init-font '("JuliaMono"
+               "Julia Mono"
+               "CascadiaCode"
+               "Cascadia Code"
+               "NotoMono"
+               "Noto Mono")))
 
 ;; Theming
-(setq onehalf-light-colors
-      '((black . "#383a42")
-        (red . "#e45649")
+(setq onehalf-light-swatches
+      '((red . "#e45649")
         (green . "#50a14f")
         (yellow . "#c18401")
         (blue . "#0184bc")
         (magenta . "#a626a4")
         (cyan . "#0997b3")
-        (white . "#fafafa")
         (comment . "#a0a1a7")
+        (selection . "#bfceff")
         (foreground . "#383a42")
-        (background . "#fafafa")
-        (selection . "#bfceff")))
-(defun init-bg-color (face color)
-  (set-face-background face (alist-get color onehalf-light-colors)))
-(defun init-fg-color (face color)
-  (set-face-foreground face (alist-get color onehalf-light-colors)))
-(when (display-graphic-p)
-  (init-bg-color 'default 'background)
-  (init-fg-color 'default 'foreground))
-(init-fg-color 'font-lock-comment-delimiter-face 'comment)
-(init-fg-color 'font-lock-comment-face 'comment)
-(init-fg-color 'font-lock-doc-face 'comment)
-(init-fg-color 'font-lock-builtin-face 'magenta)
-(init-fg-color 'font-lock-constant-face 'cyan)
-(init-fg-color 'font-lock-function-name-face 'blue)
-(init-fg-color 'font-lock-keyword-face 'red)
-(init-fg-color 'font-lock-preprocessor-face 'yellow)
-(init-fg-color 'font-lock-string-face 'green)
-(init-fg-color 'font-lock-type-face 'yellow)
-(init-fg-color 'font-lock-variable-name-face 'foreground)
-(init-bg-color 'region 'selection)
+        (background . "#fafafa")))
+(setq terminal-base-swatches
+      '((red . "red")
+        (green . "green")
+        (yellow . "yellow")
+        (blue . "blue")
+        (magenta . "magenta")
+        (cyan . "cyan")
+        (comment . "brightblack")
+        (selection . "brightblue")))
+(setq swatches
+      (if (display-graphic-p) onehalf-light-swatches terminal-base-swatches))
+(defun swatch-to-color (swatch) (alist-get swatch swatches))
+(defun init-face-bg (face swatch)
+  (let ((color (swatch-to-color swatch)))
+    (when color (set-face-background face color))))
+(defun init-face-fg (face swatch)
+  (let ((color (swatch-to-color swatch)))
+    (when color (set-face-foreground face color))))
+
+;; Faces
+(init-face-bg 'default 'background)
+(init-face-fg 'default 'foreground)
+(init-face-fg 'font-lock-comment-delimiter-face 'comment)
+(init-face-fg 'font-lock-comment-face 'comment)
+(init-face-fg 'font-lock-doc-face 'comment)
+(init-face-fg 'font-lock-builtin-face 'magenta)
+(init-face-fg 'font-lock-constant-face 'cyan)
+(init-face-fg 'font-lock-function-name-face 'blue)
+(init-face-fg 'font-lock-keyword-face 'red)
+(init-face-fg 'font-lock-preprocessor-face 'yellow)
+(init-face-fg 'font-lock-string-face 'green)
+(init-face-fg 'font-lock-type-face 'yellow)
+(init-face-fg 'font-lock-variable-name-face 'foreground)
+(init-face-bg 'region 'selection)
 
 ;; C/C++
 (setq c-default-style "stroustrup")
