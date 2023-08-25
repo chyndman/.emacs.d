0d730cca3791965d4d86055f763a7310689faf80
Chris Hyndman <chrishyndman@hey.com>
2023-08-25 10:34:20 -0700

    doom-themes opera

--- init.el
+++ init.el
@@ -39,62 +39,11 @@
                "NotoMono"
                "Noto Mono")))
 
-;; Theming
-(setq onehalf-light-swatches
-      '((red . "#e45649")
-        (green . "#50a14f")
-        (yellow . "#c18401")
-        (blue . "#0184bc")
-        (magenta . "#a626a4")
-        (cyan . "#0997b3")
-        (comment . "#a0a1a7")
-        (selection . "#bfceff")
-        (foreground . "#383a42")
-        (background . "#fafafa")))
-(setq terminal-base-swatches
-      '((red . "red")
-        (green . "green")
-        (yellow . "yellow")
-        (blue . "blue")
-        (magenta . "magenta")
-        (cyan . "cyan")
-        (comment . "brightblack")
-        (selection . "brightblue")))
-(setq swatches
-      (if (display-graphic-p) onehalf-light-swatches terminal-base-swatches))
-(defun swatch-to-color (swatch) (alist-get swatch swatches))
-(defun init-face-bg (face swatch)
-  (let ((color (swatch-to-color swatch)))
-    (when color (set-face-background face color))))
-(defun init-face-fg (face swatch)
-  (let ((color (swatch-to-color swatch)))
-    (when color (set-face-foreground face color))))
-
-;; Faces
-(init-face-bg 'default 'background)
-(init-face-fg 'default 'foreground)
-(init-face-fg 'font-lock-comment-delimiter-face 'comment)
-(init-face-fg 'font-lock-comment-face 'comment)
-(init-face-fg 'font-lock-doc-face 'comment)
-(init-face-fg 'font-lock-builtin-face 'magenta)
-(init-face-fg 'font-lock-constant-face 'cyan)
-(init-face-fg 'font-lock-function-name-face 'blue)
-(init-face-fg 'font-lock-keyword-face 'red)
-(init-face-fg 'font-lock-preprocessor-face 'yellow)
-(init-face-fg 'font-lock-string-face 'green)
-(init-face-fg 'font-lock-type-face 'yellow)
-(set-face-foreground 'font-lock-variable-name-face nil)
-(init-face-bg 'region 'selection)
-(when (not (display-graphic-p))
-  (init-face-fg 'minibuffer-prompt 'blue))
-
-;; C/C++
-(setq c-default-style "stroustrup")
-
 ;; contrib load paths
 (let ((default-directory (concat user-emacs-directory "contrib")))
   (normal-top-level-add-to-load-path
-   '("async"
+   '("doom-themes"
+     "async"
      "popup"
      "helm"
      "markdown-mode"
@@ -116,10 +65,9 @@
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-h SPC") 'helm-all-mark-rings)
 (helm-mode 1)
-(when (not (display-graphic-p))
-  (set-face-background 'helm-selection "brightgreen")
-  (set-face-background 'helm-selection-line "brightgreen")
-  (set-face-background 'helm-source-header "brightblue"))
+
+;; C/C++
+(setq c-default-style "stroustrup")
 
 ;; Markdown
 (require 'markdown-mode)
@@ -134,3 +82,28 @@
 ;; Rust
 (autoload 'rust-mode "rust-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+
+;; GUI Theme
+(when (display-graphic-p)
+  (require 'doom-themes)
+  (load-theme 'doom-opera-light t))
+
+;; Terminal Theme
+(when (not (display-graphic-p))
+  (set-face-foreground 'font-lock-comment-delimiter-face "brightblack")
+  (set-face-foreground 'font-lock-comment-face "brightblack")
+  (set-face-foreground 'font-lock-doc-face "brightblack")
+  (set-face-foreground 'font-lock-builtin-face "cyan")
+  (set-face-foreground 'font-lock-constant-face "magenta")
+  (set-face-foreground 'font-lock-function-name-face "cyan")
+  (set-face-foreground 'font-lock-keyword-face "blue")
+  (set-face-foreground 'font-lock-preprocessor-face "blue")
+  (set-face-foreground 'font-lock-string-face "green")
+  (set-face-foreground 'font-lock-type-face "yellow")
+  (set-face-foreground 'font-lock-variable-name-face "red")
+  (set-face-background 'region "brightblue")
+  (set-face-foreground 'minibuffer-prompt "blue")
+  (set-face-background 'helm-selection "brightgreen")
+  (set-face-background 'helm-selection-line "brightgreen")
+  (set-face-foreground 'helm-source-header "white")
+  (set-face-background 'helm-source-header "brightblue"))
