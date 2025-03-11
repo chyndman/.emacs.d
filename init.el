f40cd561e209a3a49f14f0a1a13a78df88c9331a
Chris Hyndman <chrishyndman@hey.com>
2025-03-10 23:12:59 -0700

    swiper default

--- init.el
+++ init.el
@@ -47,7 +47,7 @@
 ;; Ivy/Counsel/Swiper
 (ivy-mode 1)
 (counsel-mode 1)
-(global-set-key (kbd "C-s") 'swiper-isearch)
+(global-set-key (kbd "C-s") 'swiper)
 (global-set-key (kbd "C-r") 'swiper-backward)
 
 ;; C/C++
