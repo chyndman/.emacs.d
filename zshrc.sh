63ba7dcd84807224faf97a2c59767cd465bfbc20
Chris Hyndman <chrishyndman@hey.com>
2026-04-30 07:52:16 -0700

    zshrc.sh

---
+++ zshrc.sh
@@ -0,0 +1,16 @@
+alias ecn="emacsclient -n"
+alias ect="emacsclient -t"
+alias enw="emacs -nw"
+
+which -s emacsclient && export EDITOR="$(which -s emacsclient) -t --alternate-editor="
+
+autoload -Uz add-zsh-hook
+_precmd_title() { print -Pn "\e]0;%1~\a" }
+add-zsh-hook precmd _precmd_title
+
+autoload -Uz vcs_info
+precmd() { vcs_info }
+zstyle ':vcs_info:git:*' formats ' (%b)'
+setopt PROMPT_SUBST
+NEWLINE=$'\n'
+PROMPT='%F{green}%n@%m%f %F{yellow}%~%f%F{cyan}${vcs_info_msg_0_}%f${NEWLINE}$ '
